e7a3ac2275996e9c390d03a0437b4146029db59c
aymencharaf <104837875+aymencharaf@users.noreply.github.com>
2026-09-11 18:43:05 +0100

    Update config.php

--- public/config.php
+++ public/config.php
@@ -2,13 +2,14 @@
 
 return [
     'host' => 'sql203.infinityfree.com',
-    'port' => 3306,
     'dbname' => 'if0_42891786_aymen',
     'user' => 'if0_42891786',
-    'pass' => 'كلمة_مرور_قاعدة_البيانات',
+
+    // ضع كلمة مرور MySQL الجديدة هنا
+    'pass' => 'YOUR_MYSQL_PASSWORD',
 
     'admin_username' => 'aymen',
-    'admin_password' => 'aymen72874722',
+    'admin_password' => 'ay120012',
 
     'gemini_api_key' => '',
 ];

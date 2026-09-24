1c06a0a98add9fa5296677d0b6dfeb761164b0e2
aymencharaf <104837875+aymencharaf@users.noreply.github.com>
2026-09-24 10:03:39 +0100

    Update api.php

--- public/api.php
+++ public/api.php
@@ -163,37 +163,7 @@ session_set_cookie_params([
 session_start();
 
 /*
-|--------------------------------------------------------------------------
-| Runtime error protection
-|--------------------------------------------------------------------------
-*/
 
-register_shutdown_function(function () {
-
-    $e = error_get_last();
-
-    if (
-        $e &&
-        in_array(
-            $e['type'],
-            [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR],
-            true
-        )
-    ) {
-        if (!headers_sent()) {
-            header('Content-Type: application/json; charset=utf-8');
-        }
-
-        http_response_code(500);
-
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'حدث خطأ داخلي في الخادم.'
-        ], JSON_UNESCAPED_UNICODE);
-    }
-});
-
-/*
 |--------------------------------------------------------------------------
 | Helpers
 |--------------------------------------------------------------------------
